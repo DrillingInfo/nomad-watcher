52d8ecdf81aaa10c8805d8a25cd549a9d8af3bfe
Kevin Daniels <kevin.daniels@drillinginfo.com>
2018-10-15 12:55:34 -0500

    Add job metadata to alloc events

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
     "fmt"
     "syscall"
     "encoding/json"
-    
+
     flags "github.com/jessevdk/go-flags"
     log "github.com/Sirupsen/logrus"
-    
+
     nomad "github.com/hashicorp/nomad/api"
-    
-    "github.com/bsd/nomad-watcher/watcher"
+
+    "github.com/Drillinginfo/nomad-watcher/watcher"
 )
 
 var version string = "undef"
@@ -24,36 +24,36 @@ type Options struct {
 
 func main() {
     var opts Options
-    
+
     _, err := flags.Parse(&opts)
     if err != nil {
         os.Exit(1)
     }
-    
+
     if opts.Debug {
         log.SetLevel(log.DebugLevel)
     }
-    
+
     if opts.LogFile != "" {
         logFp, err := os.OpenFile(opts.LogFile, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0600)
         checkError(fmt.Sprintf("error opening %s", opts.LogFile), err)
-        
+
         defer logFp.Close()
-        
+
         // ensure panic output goes to log file
         syscall.Dup2(int(logFp.Fd()), 1)
         syscall.Dup2(int(logFp.Fd()), 2)
-        
+
         // log as JSON
         log.SetFormatter(&log.JSONFormatter{})
-        
+
         // send output to file
         log.SetOutput(logFp)
     }
-    
+
     log.Debug("hi there! (tickertape tickertape)")
     log.Infof("version: %s", version)
-    
+
     evtsFp, err := os.OpenFile(opts.EventFile, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0600)
     checkError(fmt.Sprintf("error opening %s", opts.EventFile), err)
 
@@ -61,42 +61,42 @@ func main() {
 
     nomadClient, err := nomad.NewClient(nomad.DefaultConfig())
     checkError("creating Nomad client", err)
-    
+
     enc := json.NewEncoder(evtsFp)
-    
+
     eventChan := make(chan interface{})
-    
-    allocEventChan, taskStateEventChan := watcher.WatchAllocations(nomadClient.Allocations())
+
+    allocEventChan, taskStateEventChan := watcher.WatchAllocations(nomadClient.Allocations(), nomadClient.Jobs())
     go func() {
         for ae := range allocEventChan {
             eventChan <- ae
         }
     }()
-    
+
     go func() {
         for tse := range taskStateEventChan {
             eventChan <- tse
         }
     }()
-    
+
     go func() {
         for ee := range watcher.WatchEvaluations(nomadClient.Evaluations()) {
             eventChan <- ee
         }
     }()
-    
+
     go func() {
         for je := range watcher.WatchJobs(nomadClient.Jobs()) {
             eventChan <- je
         }
     }()
-    
+
     go func() {
         for ne := range watcher.WatchNodes(nomadClient.Nodes()) {
             eventChan <- ne
         }
     }()
-    
+
     for e := range eventChan {
         checkError("serializing event", enc.Encode(e))
     }
--- watcher/allocations.go
+++ watcher/allocations.go
@@ -11,13 +11,14 @@ type AllocEvent struct {
     Timestamp  time.Time                       `json:"@timestamp"`
     WaitIndex  uint64                          `json:"wait_index"`
     AllocationListStub *api.AllocationListStub `json:"alloc"`
+    JobMeta  map[string]string                 `json:"job_meta"`
 }
 
 // this is a derived event from an allocation. Time comes from TaskEvent.Time.
 type TaskStateEvent struct {
     Timestamp  time.Time `json:"@timestamp"`
     WaitIndex  uint64    `json:"wait_index"`
-    
+
     // copied from the Allocation
     JobID              string // "elasticsearch-curator/periodic-1488845700"
     AllocID            string // "29cdfa9e-820a-bab8-4eda-45b000397719"
@@ -25,93 +26,102 @@ type TaskStateEvent struct {
     TaskGroup          string // "curator"
     EvalID             string // "8eb39148-7a00-a164-920f-d59143f72b74"
     NodeID             string // "f8fa01e0-351b-7652-d80e-10547fa3bfe8"
-    
+
     // Task comes from the keys of TaskStates
     Task string
-    
+
     // State and Failed come from the TaskState
     State  string
     Failed bool
-    
+
     // the actual Task Event (phew!)
     TaskEvent *api.TaskEvent
 }
 
-func WatchAllocations(allocClient *api.Allocations) (<- chan AllocEvent, <- chan TaskStateEvent) {
+func WatchAllocations(allocClient *api.Allocations, jobsClient *api.Jobs) (<- chan AllocEvent, <- chan TaskStateEvent) {
     log := logrus.WithFields(logrus.Fields{
         "package": "watcher",
         "fn": "WatchAllocations",
     })
-    
+
     allocEventChan := make(chan AllocEvent)
     taskStateEventChan := make(chan TaskStateEvent)
-    
+
     keepWatching := true
-    
+
     go func() {
         // keeping track of the most recently-seen task state event (TaskEvent)
         // timestamp for each allocation.  also allows for tracking allocations
         // that are removed.
         // @todo possibly need to track per-task times?
         allocTaskEventTimes := make(map[string]int64)
-        
+
         queryOpts := &api.QueryOptions{
             WaitTime: 1 * time.Minute,
         }
-        
+
         for keepWatching {
             log.Debugf("retrieving from index %d", queryOpts.WaitIndex)
             allocStubs, queryMeta, err := allocClient.List(queryOpts)
-            
+
             if err != nil {
                 log.Errorf("unable to list allocations: %v", err)
                 continue
             }
-            
+
             if queryOpts.WaitIndex > 0 {
                 // only emit events after the first run; we're looking for
                 // changes
-                
+
                 // the time when the result was retrieved
                 now := time.Now()
-                
+
                 // map of allocation ids in the current result
                 extantAllocs := make(map[string]bool)
-                
+
                 for _, allocStub := range allocStubs {
                     ts := now
-                    
+
                     allocId := allocStub.ID
                     extantAllocs[allocId] = true
-                    
+
+
                     if (allocStub.CreateIndex == allocStub.ModifyIndex) {
                         // allocation just created; use CreateTime field
                         ts = time.Unix(0, allocStub.CreateTime)
                     }
-                    
+
                     // true if allocation was created or updated
                     allocationUpdated := (queryOpts.WaitIndex < allocStub.CreateIndex) || (queryOpts.WaitIndex < allocStub.ModifyIndex)
-                    
+
                     if allocationUpdated {
+                        //If alloc is updated, query it's Job so the Metadata can be extracted
+                        jobInfo, _, err := jobsClient.Info(allocStub.JobID, queryOpts)
+                        if err != nil {
+                            log.Errorf("unable to find job: %v", err)
+                            continue
+                        }
+                      
                         allocEventChan <- AllocEvent{
                             ts,
                             queryMeta.LastIndex,
                             allocStub,
+                            jobInfo.Meta,
                         }
                     }
-                    
+
                     lastTaskEventTime := allocTaskEventTimes[allocId]
                     for taskName, taskState := range allocStub.TaskStates {
                         for _, taskEvent := range taskState.Events {
                             if taskEvent.Time > lastTaskEventTime {
                                 // new TaskEvent
-                                
+
                                 // emit only if the allocation was updated
                                 if allocationUpdated {
                                     taskStateEventChan <- TaskStateEvent{
                                         Timestamp: time.Unix(0, taskEvent.Time),
                                         WaitIndex: queryMeta.LastIndex,
-                                        
+
                                         JobID:     allocStub.JobID,
                                         AllocID:   allocId,
                                         AllocName: allocStub.Name,
@@ -122,34 +132,34 @@ func WatchAllocations(allocClient *api.Allocations) (<- chan AllocEvent, <- chan
                                         Task:   taskName,
                                         State:  taskState.State,
                                         Failed: taskState.Failed,
-                                        
+
                                         TaskEvent: taskEvent,
-                                        
+
                                     }
                                 }
 
                                 // store the timestamp of the most recent task
-                                // event; assumption is taskState.Events is always ordered 
+                                // event; assumption is taskState.Events is always ordered
                                 allocTaskEventTimes[allocId] = taskEvent.Time
                             }
                         }
                     }
                 }
-                
+
                 // prune allocs
                 for allocId, _ := range allocTaskEventTimes {
                     if _, ok := extantAllocs[allocId]; ! ok {
                         log.Infof("allocation %s has been deleted", allocId)
-                        
+
                         delete(allocTaskEventTimes, allocId)
                     }
                 }
-                
+
             }
-            
+
             queryOpts.WaitIndex = queryMeta.LastIndex
         }
     }()
-    
+
     return allocEventChan, taskStateEventChan
 }
